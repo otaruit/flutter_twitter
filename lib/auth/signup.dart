9bcfd10642e17300c60fdb8efb84320c792d0d25
natsumekimura <kimuranatsume5656@gmail.com>
2023-10-23 12:10:34 +0900

    twitter_2途中

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -8,13 +9,57 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  void signUpAction() async {
+    try {
+      UserCredential userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: "barry.allen@example.com",
+              password: "SuperSecretPassword!");
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  String email = '';
+  String password = '';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      backgroundColor: Colors.blue,
-      elevation: 8,
-      title: Text("Sign up"),
-    ));
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        elevation: 8,
+        title: const Text("Sign up"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+        child: Form(
+            child: Column(
+          children: [
+            TextFormField(
+              onChanged: (val) => setState(() {
+                email = val;
+              }),
+            ),
+            TextFormField(
+              onChanged: (val) => setState(() {
+                password = val;
+              }),
+            ),
+            ElevatedButton(
+                onPressed: () async => {signUpAction()},
+                child: const Text('Sign up'))
+          ],
+        )),
+      ),
+    );
   }
 }
